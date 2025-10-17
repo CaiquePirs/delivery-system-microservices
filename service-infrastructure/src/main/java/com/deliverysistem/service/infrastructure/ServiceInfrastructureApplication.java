c8220c0a76406c94931316e33766c10d7b6d31ea
Caique Pires <c.pires@ba.estudante.senai.br>
2025-10-17 15:40:26 +0100

    feat(infra): Implements docker-compose with databases config and diagrams system

---
+++ service-infrastructure/src/main/java/com/deliverysistem/service/infrastructure/ServiceInfrastructureApplication.java
@@ -0,0 +1,13 @@
+package com.deliverysistem.service.infrastructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceInfrastructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceInfrastructureApplication.class, args);
+	}
+
+}
---
+++ service-infrastructure/src/test/java/com/deliverysistem/service/infrastructure/ServiceInfrastructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deliverysistem.service.infrastructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceInfrastructureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

ca1a82de56d767bafb77e933c42925396c89912e
Caique Pires <c.pires@ba.estudante.senai.br>
2025-10-21 17:13:36 +0100

    feat(orders): Implements custom error handling for requests not found

--- orders-service/src/main/java/com/deliverysystem/orders/controller/advice/handler/GlobalExceptionHandler.java
+++ orders-service/src/main/java/com/deliverysystem/orders/controller/advice/handler/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.deliverysystem.orders.controller.advice.handler;
 import com.deliverysystem.orders.controller.advice.dto.ErrorMessageDTO;
 import com.deliverysystem.orders.controller.advice.dto.ErrorResponseDTO;
 import com.deliverysystem.orders.controller.exception.ClientNotFoundException;
+import com.deliverysystem.orders.controller.exception.OrderNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -35,6 +36,17 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(ClientNotFoundException.class)
     public ResponseEntity<ErrorResponseDTO> handleClientNotFound(ClientNotFoundException e){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ErrorResponseDTO(
+                        HttpStatus.NO_CONTENT.value(),
+                        e.getMessage(),
+                        LocalDateTime.now(),
+                        List.of(new ErrorMessageDTO("Not Found", e.getMessage()))
+                ));
+    }
+
+    @ExceptionHandler(OrderNotFoundException.class)
+    public ResponseEntity<ErrorResponseDTO> handleOrderNotFound(OrderNotFoundException e){
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(new ErrorResponseDTO(
                         HttpStatus.NO_CONTENT.value(),

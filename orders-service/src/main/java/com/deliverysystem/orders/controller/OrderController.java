f0a8da8d0b4ef8da8ff5427ae85842abcc81fcee
Caique Pires <c.pires@ba.estudante.senai.br>
2025-10-23 23:42:54 +0100

    refactor(order):

--- orders-service/src/main/java/com/deliverysystem/orders/controller/OrderController.java
+++ orders-service/src/main/java/com/deliverysystem/orders/controller/OrderController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.*;
 public class OrderController {
 
     private final OrderService orderService;
-    private final OrderMapper orderMapper;
 
     @PostMapping
     public ResponseEntity<OrderResponseDTO> createOrder(@RequestBody @Valid OrderRequestDTO dto){

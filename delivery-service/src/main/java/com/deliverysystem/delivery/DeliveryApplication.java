26ff6911629e3710d6e12ff3b09380861c0fc0df
Caique Pires <c.pires@ba.estudante.senai.br>
2025-10-25 13:26:00 +0100

    refactor(delivery): Implement rabbitmq config

--- delivery-service/src/main/java/com/deliverysystem/delivery/DeliveryApplication.java
+++ delivery-service/src/main/java/com/deliverysystem/delivery/DeliveryApplication.java
@@ -2,8 +2,10 @@ package com.deliverysystem.delivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.deliverysystem.delivery.client.api")
 public class DeliveryApplication {
 
     public static void main(String[] args) {

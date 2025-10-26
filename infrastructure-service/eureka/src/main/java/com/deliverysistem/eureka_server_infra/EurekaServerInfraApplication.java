bc3a6b12b86b0d112f873547a5f8b204aef8db7a
Caique Pires <c.pires@ba.estudante.senai.br>
2025-10-26 15:19:44 +0000

    refactor(infra) Implement eureka-service in the infrastructure model to handle service discovery separately from the database infrastructure module and the rabbitmq broker.

--- infrastructure-service/eureka/src/main/java/com/deliverysistem/eureka_server_infra/EurekaServerInfraApplication.java
+++ infrastructure-service/eureka/src/main/java/com/deliverysistem/eureka_server_infra/EurekaServerInfraApplication.java
@@ -1,15 +1,15 @@
-package com.deliverysistem.service.infrastructure;
+package com.deliverysistem.eureka_server_infra;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
 @SpringBootApplication
-public class ServiceInfrastructureApplication {
+@EnableEurekaServer
+public class EurekaServerInfraApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServiceInfrastructureApplication.class, args);
+		SpringApplication.run(EurekaServerInfraApplication.class, args);
 	}
 
 }

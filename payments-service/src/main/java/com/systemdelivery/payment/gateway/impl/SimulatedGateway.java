8c96de01751cde6f6cf3339d572883f3218c6e91
Caique Pires <c.pires@ba.estudante.senai.br>
2025-11-11 19:28:56 +0000

    feat(payments): Implements security and validation flow to perform the webhook for an order payment
    - Adds validation methods to verify whether it is a service with permission to perform the request in the payment service
    
    - Implements a method to retrieve the token from the authentication service before performing the webhook request

--- payments-service/src/main/java/com/systemdelivery/payment/gateway/impl/SimulatedGateway.java
+++ payments-service/src/main/java/com/systemdelivery/payment/gateway/impl/SimulatedGateway.java
@@ -2,15 +2,21 @@ package com.systemdelivery.payment.gateway.impl;
 
 import com.systemdelivery.payment.gateway.dto.PaymentWebhookDTO;
 import com.systemdelivery.payment.model.enums.PaymentStatus;
+import com.systemdelivery.payment.service.TokenClientService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.http.*;
 import java.time.Duration;
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 @Slf4j
 public class SimulatedGateway {
+
+    private final TokenClientService tokenClientService;;
 
     public void simulateCallback(String paymentId){
         // Simulate a delay for payment processing
@@ -30,10 +36,21 @@ public class SimulatedGateway {
 
         try {
             RestTemplate restTemplate = new RestTemplate();
-            restTemplate.postForEntity("http://localhost:8080/api/payments/webhook", webhook, Void.class);
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            headers.setBearerAuth(tokenClientService.getAccessToken());
+
+            HttpEntity<PaymentWebhookDTO> request = new HttpEntity<>(webhook, headers);
+
+            ResponseEntity<Void> callbackWebhook = restTemplate.exchange(
+                    "http://localhost:8080/api/payments/webhook",
+                    HttpMethod.POST,
+                    request,
+                    Void.class
+            );
 
         } catch (Exception e){
-            log.error("Error when simulating payment callback: {}", e.getMessage());
+            log.error("Error when simulating payment callback: {}, for payment ID: {}", e.getMessage(), paymentId);
         }
     }
 }

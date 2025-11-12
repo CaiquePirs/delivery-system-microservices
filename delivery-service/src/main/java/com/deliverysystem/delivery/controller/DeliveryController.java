104c0edd67b00d80d18ccd8f757544613ccd571f
Caique Pires <c.pires@ba.estudante.senai.br>
2025-11-12 19:05:23 +0000

    feat(delivery): Implements logic with validation when performing the delivery webhook callback
    - validates whether the restaurant is the owner of the resource it is attempting to perform the callback on
    
    - implements strict validations to validate the delivery service

--- delivery-service/src/main/java/com/deliverysystem/delivery/controller/DeliveryController.java
+++ delivery-service/src/main/java/com/deliverysystem/delivery/controller/DeliveryController.java
@@ -3,8 +3,8 @@ package com.deliverysystem.delivery.controller;
 import com.deliverysystem.delivery.service.DeliveryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.UUID;
 
 @RestController
@@ -15,6 +15,7 @@ public class DeliveryController {
     private final DeliveryService deliveryService;
 
     @PostMapping("/{id}")
+    @PreAuthorize("hasRole('RESTAURANT')")
     public ResponseEntity<Void> receiveDeliveryUpdate(@PathVariable("id") UUID deliveryId){
         deliveryService.callbackDeliveryReady(deliveryId);
         return ResponseEntity.noContent().build();
--- delivery-service/src/main/java/com/deliverysystem/delivery/service/DeliveryService.java
+++ delivery-service/src/main/java/com/deliverysystem/delivery/service/DeliveryService.java
@@ -1,5 +1,6 @@
 package com.deliverysystem.delivery.service;
 
+import com.deliverysystem.delivery.client.representation.OrderDTO;
 import com.deliverysystem.delivery.controller.advice.exceptions.DeliveryErrorException;
 import com.deliverysystem.delivery.controller.advice.exceptions.NotFoundException;
 import com.deliverysystem.delivery.client.ClientApiService;
@@ -10,9 +11,11 @@ import com.deliverysystem.delivery.model.Delivery;
 import com.deliverysystem.delivery.model.enums.DeliveryStatus;
 import com.deliverysystem.delivery.repositories.DeliveryRepository;
 import com.deliverysystem.delivery.service.calculator.DeliveryTaxCalculator;
+import com.deliverysystem.delivery.validator.DeliveryValidator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -27,13 +30,13 @@ public class DeliveryService {
     private final CurrierService currierService;
     private final DeliveryTaxCalculator deliveryTaxCalculator;
     private final DeliveryEventPublisher deliveryEventPublisher;
-    private final SimulatedCallbackDelivery simulatedCallbackDelivery;
+    private final DeliveryValidator deliveryValidator;
 
     public void processDeliveryForOrder(PaymentApprovedEvent event) {
         var order = clientApiService.findById(event.orderId());
 
-        if (order == null) {
-            log.error("Order with ID {} not found.", event.orderId());
+        if(order == null){
+            log.error("Error processing delivery for order ID: {}", event.orderId());
             return;
         }
 
@@ -46,12 +49,17 @@ public class DeliveryService {
                 .build();
 
         deliveryRepository.save(delivery);
-        simulatedCallbackDelivery.simulateWebhook(delivery.getId());
     }
 
+    @Transactional
     public void callbackDeliveryReady(UUID deliveryId) {
-        Delivery delivery = deliveryRepository.findById(deliveryId)
-                .orElseThrow(() -> new NotFoundException("Delivery not found with ID: " + deliveryId));
+        Delivery delivery = findDeliveryById(deliveryId);
+        OrderDTO order = clientApiService.findById(delivery.getOrderId());
+
+        if (order == null) {
+            throw new NotFoundException("error processing delivery with ID: " + deliveryId + "order information is not found");
+        }
+        deliveryValidator.validateAuthenticatedRestaurantOwnership(order.restaurantId());
 
         if (delivery.getStatus().equals(DeliveryStatus.ASSIGNED)) {
             Currier currier = currierService.findAvailableCourierForDelivery();
@@ -67,13 +75,12 @@ public class DeliveryService {
             deliveryRepository.save(delivery);
             deliveryEventPublisher.publishDeliveryShipped(delivery);
 
-        } else {
-            throw new DeliveryErrorException(
-                    String.format("Error processing delivery ID: %s , the current delivery status is: %s",
-                            deliveryId,
-                            delivery.getStatus())
-            );
-        }
+        } else throw new DeliveryErrorException(deliveryId, delivery.getStatus());
+    }
+
+    private Delivery findDeliveryById(UUID deliveryId) {
+        return deliveryRepository.findById(deliveryId)
+                .orElseThrow(() -> new NotFoundException("Delivery not found with ID: " + deliveryId));
     }
 
 }
